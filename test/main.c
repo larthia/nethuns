cfcd9b75d939080c308a7e242e2071dbd5a8916a
Nicola Bonelli <nicola.bonelli@gmail.com>
2018-10-23 15:23:31 +0000

    Test updated.

--- test/main.c
+++ test/main.c
@@ -1,6 +1,19 @@
 #include <nethuns.h>
 #include <stdio.h>
 
+void dump_packet(nethuns_pkthdr_t *hdr, unsigned char *frame)
+{
+    int i = 0;
+
+    printf("%u:%u snap:%u len:%u mac:%u", hdr->tp_sec, hdr->tp_nsec, hdr->tp_snaplen, hdr->tp_len, hdr->tp_mac);
+    for(; i < 14; i++)
+    {
+        printf("%02x ", frame[i]);
+    }
+    printf("\n");
+}
+
+
 int
 main(int argc, char *argv[])
 {
@@ -22,7 +35,7 @@ main(int argc, char *argv[])
 
         if ((block = nethuns_recv(s, &pkthdr, &frame)))
         {
-            printf("packet!\n");
+            dump_packet(pkthdr, frame);
             nethuns_release(s, pkthdr, block, 0);
         }
 
