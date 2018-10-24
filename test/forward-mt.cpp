340cb9c5784a17f542b57117bd9adb687d9556bb
Nicola Bonelli <nicola.bonelli@gmail.com>
2018-10-24 13:17:08 -0400

    Nethuns forward mt example.

---
+++ test/forward-mt.cpp
@@ -0,0 +1,113 @@
+#include <boost/lockfree/spsc_queue.hpp>
+#include <nethuns.h>
+#include <stdio.h>
+
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <iostream>
+
+
+std::atomic_long total;
+
+void meter()
+{
+    auto now = std::chrono::system_clock::now();
+    for(;;)
+    {
+        now += std::chrono::seconds(1);
+        std::this_thread::sleep_until(now);
+        auto x = total.exchange(0);
+        std::cout << "pkt/sec: " << x << std::endl;
+    }
+}
+
+
+struct nethuns_pkt
+{
+    nethuns_socket_t  sock;
+    nethuns_pkthdr_t  *pkthdr;
+    uint64_t          block;
+
+};
+
+
+boost::lockfree::spsc_queue<nethuns_pkt> queue (8192);
+
+
+int consumer(std::string dev)
+{
+    nethuns_socket_t out = nethuns_open( 4        /* number of blocks */
+                                       , 4        /* packets per block */
+                                       , 2048     /* max packet size */
+                                       );
+
+    if (nethuns_bind(out, dev.c_str()) < 0)
+    {
+        return -1;
+    }
+
+
+    for(;;)
+    {
+        nethuns_pkt pkt;
+
+        if (queue.pop(pkt)) {
+            total++;
+
+            while (!nethuns_send(out, (uint8_t *)pkt.pkthdr + pkt.pkthdr->tp_mac, pkt.pkthdr->tp_len))
+            { };
+
+            nethuns_release(pkt.sock, pkt.pkthdr, pkt.block, 0);
+        }
+    }
+
+    nethuns_close(out);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        std::cerr << "usage: " << argv[0] << " in out" << std::endl;
+        return 0;
+    }
+
+    std::thread(meter).detach();
+
+    std::thread(consumer, std::string{argv[2]}).detach();
+
+    nethuns_socket_t s = nethuns_open( 4        /* number of blocks */
+                                     , 65536    /* packets per block */
+                                     , 2048     /* max packet size */
+                                     );
+
+    if (nethuns_bind(s, argv[1]) < 0)
+    {
+        return -1;
+    }
+
+    unsigned char *frame;
+    nethuns_pkthdr_t *pkthdr;
+
+    nethuns_set_consumer(s, 1);
+
+    for(;;)
+    {
+        uint64_t block;
+
+        if ((block = nethuns_recv(s, &pkthdr, &frame)))
+        {
+            nethuns_pkt p { s, pkthdr, block };
+
+            while (!queue.push(p))
+            { };
+        }
+    }
+
+    nethuns_close(s);
+    return 0;
+}
+
