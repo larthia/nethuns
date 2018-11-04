a38abdd947d2da808412bead8c20e979f173e8ad
Nicola Bonelli <nicola.bonelli@gmail.com>
2018-11-04 08:24:47 +0000

    synapse header dismissed.

--- src/nethuns/internals/compiler.h
+++ src/nethuns/internals/compiler.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#define MAX(a,b) \
+        ({ __typeof__ (a) _a = (a); \
+               __typeof__ (b) _b = (b); \
+                     _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+        ({ __typeof__ (a) _a = (a); \
+               __typeof__ (b) _b = (b); \
+                     _a < _b ? _a : _b; })
+
 #ifndef likely
 #define likely(x) __builtin_expect((x),1)
 #endif
