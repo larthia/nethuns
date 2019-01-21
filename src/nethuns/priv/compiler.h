05d9b52af87be1de2f5f3e04b7c7baacd3e7df26
Nicola Bonelli <nicola.bonelli@gmail.com>
2019-01-21 14:36:14 +0100

    Minor bugs fixed.

--- src/nethuns/priv/compiler.h
+++ src/nethuns/priv/compiler.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#ifndef MAX
 #define MAX(a,b) \
         ({ __typeof__ (a) _a = (a); \
                __typeof__ (b) _b = (b); \
                      _a > _b ? _a : _b; })
+#endif
 
+#ifndef MIN
 #define MIN(a,b) \
         ({ __typeof__ (a) _a = (a); \
                __typeof__ (b) _b = (b); \
                      _a < _b ? _a : _b; })
+#endif
 
 #ifndef likely
 #define likely(x) __builtin_expect((x),1)
@@ -18,11 +22,11 @@
 #define unlikely(x) __builtin_expect((x),0)
 #endif
 
-#ifndef __cachedline_aligned
+#ifndef __cacheline_aligned
 #define __cacheline_aligned     __attribute__((aligned(64)))
 #endif
 
-#ifndef __maybe_unused
+#ifndef  __maybe_unused
 # define __maybe_unused     __attribute__((unused))
 #endif
 
