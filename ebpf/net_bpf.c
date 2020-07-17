1a764b04a81314c345570ed5523fd01054505376
Nicola Bonelli <nicola.bonelli@larthia.com>
2020-07-17 11:57:16 +0000

    net_bpf program added.

---
+++ ebpf/net_bpf.c
@@ -0,0 +1,17 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+struct bpf_map_def SEC(".map") xsks_map = {
+	.type = BPF_MAP_TYPE_XSKMAP,
+	.key_size = sizeof(int),
+	.value_size = sizeof(int),
+	.max_entries = 1,
+};
+
+SEC("xdp_sock")
+int xdp_sock_prog(struct xdp_md *ctx)
+{
+	return bpf_redirect_map(&xsks_map, 0, 0);
+}
+
+char _license[] SEC("license") = "GPL";
