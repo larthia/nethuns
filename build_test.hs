554e9608afd509fc5e80a4686d6fa3f6cc1af5ab
Nicola Bonelli <nicola.bonelli@gmail.com>
2021-10-08 14:04:32 +0200

    Minor changes.

--- build_test.hs
+++ build_test.hs
@@ -1,19 +1,19 @@
 #!/usr/bin/env stack
--- stack runghc --resolver lts-16.31 --package optparse-applicative --package ansi-terminal --allow-different-user --install-ghc
+-- stack runghc --resolver lts-18.9 --package optparse-applicative --package ansi-terminal --allow-different-user --install-ghc
 --
 
 {-# LANGUAGE RecordWildCards #-}
 
-import System.Exit
+import System.Exit ( ExitCode(ExitFailure) )
 import System.FilePath.Posix
-import System.Process
+import System.Process ( spawnProcess, waitForProcess )
 
 data Build = Build {
-     optBuiltinPcapReader :: Opt 
-   , optTpacketV3         :: Opt  
-   , optNetmap            :: Opt  
-   , optLibpcap           :: Opt  
-   , optXDP               :: Opt  
+     optBuiltinPcapReader :: Opt
+   , optTpacketV3         :: Opt
+   , optNetmap            :: Opt
+   , optLibpcap           :: Opt
+   , optXDP               :: Opt
 }
 
 
@@ -22,13 +22,13 @@ newtype Opt = Opt Bool
 
 
 on  = Opt True
-off = Opt False 
+off = Opt False
 
 
-(.|.) :: Opt -> Opt -> Opt 
+(.|.) :: Opt -> Opt -> Opt
 (Opt a) .|. (Opt b) = Opt (a || b)
 {-# INLINE (.|.) #-}
- 
+
 
 (.&.) :: Opt -> Opt -> Opt
 (Opt a) .&. (Opt b) = Opt (a && b)
@@ -36,14 +36,13 @@ off = Opt False
 
 
 main = do
-  let builds = [ Build{..} |  optBuiltinPcapReader <- [off, on] 
-                           ,  optTpacketV3 	   <- [off, on]
-                           ,  optNetmap    	   <- [off, on]
-                           ,  optLibpcap   	   <- [off, on]
-                           ,  optXDP       	   <- [off, on] 
-			   ,  let (Opt filter) = optTpacketV3 .|. optNetmap .|. optLibpcap .|. optXDP
-				in filter	
-			   ]
+  let builds = [ Build{..} | optBuiltinPcapReader <- [off, on]
+                             ,  optTpacketV3 <- [off, on]
+                             ,  optNetmap    <- [off, on]
+                             ,  optLibpcap   <- [off, on]
+                             ,  optXDP       <- [off, on]
+                             ,  let (Opt filter) = optTpacketV3 .|. optNetmap .|. optLibpcap .|. optXDP
+              in filter ]
 
   mapM_ testBuild builds
 
@@ -54,28 +53,26 @@ instance Show Opt where
 
 
 mkCMakeOpt :: Build -> [String]
-mkCMakeOpt Build{..} = 
+mkCMakeOpt Build{..} =
         [ "NETHUNS_OPT_BUILTIN_PCAP_READER"     <> "=" <> show optBuiltinPcapReader
         , "NETHUNS_OPT_TPACKET_V3"              <> "=" <> show optTpacketV3
         , "NETHUNS_OPT_NETMAP"                  <> "=" <> show optNetmap
         , "NETHUNS_OPT_LIBPCAP"                 <> "=" <> show optLibpcap
-        , "NETHUNS_OPT_XDP"                     <> "=" <> show optXDP ] 
+        , "NETHUNS_OPT_XDP"                     <> "=" <> show optXDP ]
 
 
 testBuild :: Build -> IO ()
 testBuild build = do
   let opts = mkCMakeOpt build
-  cmd "cmake" $ ["-B", "BUILD"] <> (("-D" <>) <$> (mkCMakeOpt build))
+  cmd "cmake" $ ["-B", "BUILD"] <> (("-D" <>) <$> mkCMakeOpt build)
   cmd "make"    ["-C", "BUILD"]
   cmd "make"    ["-C", "BUILD", "clean"]
 
 
 cmd :: FilePath -> [String] -> IO ()
 cmd cmd args = do
-  putStrLn $ "-> "<> (unwords $ cmd : args) 
+  putStrLn $ "-> "<> unwords (cmd : args)
   ec <- waitForProcess =<< spawnProcess cmd args
   case ec of
     ExitFailure n -> errorWithoutStackTrace $ "builder: " <> cmd <> " exited with code " <> show n
     _ -> return ()
-
-
