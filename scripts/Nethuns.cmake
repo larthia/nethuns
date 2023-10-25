b630d3337a662351eaf6d000b5d4f8cec3e2469f
Nicola Bonelli <nicola.bonelli@gmail.com>
2023-10-25 11:17:35 +0200

    CMake scripts improved and fixed.

--- scripts/Nethuns.cmake
+++ scripts/Nethuns.cmake
@@ -32,28 +32,39 @@ list(GET NETHUNS_SOCK_LIST 0 FIRST_SOCKET)
 
 message ("Nethuns: Available sockets: ${NETHUNS_SOCK_LIST}, default: ${FIRST_SOCKET}")
 
-set(NETHUNS_CAPTURE_SOCKET ${FIRST_SOCKET} CACHE STRING "Nethuns underlying capture engine")
+set(NETHUNS_CAPTURE_SOCKET ${FIRST_SOCKET} CACHE STRING "Nethuns underlying socket")
 set_property(CACHE NETHUNS_CAPTURE_SOCKET PROPERTY STRINGS ${NETHUNS_SOCK_LIST})
 
 set(NETHUNS_LIBRARIES)
 set(NETHUNS_INCLUDE_PATHS)
 
-list(APPEND NETHUNS_INCLUDE_PATHS /usr/local/include)
+#
+# Nethuns installation...
 
-if (APPLE)
-    find_library(NETHUNS_LIBPCAP_LIBRARY NAMES libpcap.a PATHS "/usr/local/opt/libpcap/lib" "/opt/homebrew/opt/libpcap/lib" NO_DEFAULT_PATH)
+list(APPEND NETHUNS_INCLUDE_PATHS /usr/local/include)
+list(APPEND NETHUNS_LIBRARIES /usr/local/lib/libnethuns.a)
+
+#
+# detecting libpcap installation...
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    find_library(NETHUNS_LIBPCAP_LIBRARY NAMES pcap PATHS "/usr/local/opt/libpcap/lib" "/opt/homebrew/opt/libpcap/lib" NO_DEFAULT_PATH)
     find_path(NETHUNS_LIBPCAP_INCLUDE NAMES pcap/pcap.h PATHS "/usr/local/opt/libpcap/include/" "/opt/homebrew/opt/libpcap/include" NO_DEFAULT_PATH)
-elseif(LINUX)
-    find_library(NETHUNS_LIBPCAP_LIBRARY  libpcap.a PATHS "/usr/local/lib")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+    find_library(NETHUNS_LIBPCAP_LIBRARY pcap PATHS "/usr/local/lib")
     find_path(NETHUNS_LIBPCAP_INCLUDE NAMES pcap/pcap.h PATHS "/usr/local/include")
+else()
+    message(FATAL_ERROR "${CMAKE_SYSTEM_NAME} platform not supported!")
 endif()
 
-find_package(ZLIB)
 
-list(APPEND NETHUNS_LIBRARIES /usr/local/lib/libnethuns.a)
-list(APPEND NETHUNS_LIBRARIES ${NETHUNS_LIBPCAP_LIBRARY})
 list(APPEND NETHUNS_INCLUDE_PATHS ${NETHUNS_LIBPCAP_INCLUDE})
+list(APPEND NETHUNS_LIBRARIES ${NETHUNS_LIBPCAP_LIBRARY})
 
+#
+# detecting other libraries...
+
+find_package(ZLIB)
 find_library(NETHUNS_LIBBPF_LIBRARY bpf)
 find_library(NETHUNS_LIBELF_LIBRARY elf)
 find_library(NETHUNS_NETMAP_LIBRARY netmap)
